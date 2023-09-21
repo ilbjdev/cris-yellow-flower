93d58e2e72742716a4d9bd2c4bcf32a5f8b19d52
ilbert aldama <aldama.ilbertj@gmail.com>
2023-09-21 13:55:38 -0400

    efecto agregado en titulo principal

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+const typed = new Typed('.typed', {
+    strings: [
+        'Crisney Beltrán', 
+        'Mujer Hermosa', 
+        'de Gran Corazón,', 
+        'de Alto Valor', 
+        'Sexy...', 
+        'y Crujiente como', 'Galletica'
+    ],
+    //stringsElement: '#cadena-texto',
+    typeSpeed: 75,
+    starDelay: 300,
+    backSpeed: 75,
+    //smartBackspace: true,
+    shuffle: false,
+    backDelay: 1500,
+    loop: true,
+    loopCount: false,
+    showCursor: true,
+    cursorChar: '|',
+    contentType: 'html',
+});
